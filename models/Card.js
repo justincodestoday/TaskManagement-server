de5784a65e1c4568f6878bd14a2260ad197d0b92
Justin <jcmc888@gmail.com>
2022-09-18 03:29:29 +0800

    test

--- models/Card.js
+++ models/Card.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 
 const CardSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    status: String,
-    tasks: [
-        {
-            userId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User",
-            },
-            name: String,
-            status: Boolean,
-        },
-    ],
+  name: String,
+  description: String,
+  status: String,
+  tasks: [
+    {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      name: String,
+      status: Boolean,
+    },
+  ],
 });
 
 module.exports = mongoose.model("Card", CardSchema);
+
+// testing commit
