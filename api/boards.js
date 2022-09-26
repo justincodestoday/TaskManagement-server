b620821988f3c3208083a4fd1a60e8f2cb517ecf
juju <jcmc888@gmail.com>
2022-09-26 21:56:33 +0800

    updated create board api

--- api/boards.js
+++ api/boards.js
@@ -9,250 +9,263 @@ const Board = require("../models/Board");
 
 // Add a board
 router.post("/", auth, async (req, res) => {
-  try {
-    const { title } = req.body;
+    try {
+        const { title, backgroundURL } = req.body;
 
-    if (title === "") {
-      return res.status(400).json({ message: "Title is required" });
+        if (title === "") {
+            return res.status(400).json({ message: "Title is required" });
+        }
+
+        // Create and save the board
+        const newBoard = new Board({ title, backgroundURL });
+        const board = await newBoard.save();
+
+        // Add board to user's boards
+        const user = await User.findById(req.user._id);
+        user.boards.unshift(board.id);
+        await user.save();
+
+        // Add user to board's members as admin
+        board.members.push({ user: user.id, name: user.name });
+
+        // Log activity
+        board.activity.unshift({ text: `${user.name} created this board` });
+        await board.save();
+
+        return res
+            .status(200)
+            .json({ message: "Board created successfully", board });
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ message: "Server Error" });
     }
-
-    // BACKGROUNDURL USE FILE OR URL?
-
-    // Create and save the board
-    const newBoard = new Board({ title });
-    const board = await newBoard.save();
-
-    // Add board to user's boards
-    const user = await User.findById(req.user._id);
-    user.boards.unshift(board.id);
-    await user.save();
-
-    // Add user to board's members as admin
-    board.members.push({ user: user.id, name: user.name });
-
-    // Log activity
-    board.activity.unshift({ text: `${user.name} created this board` });
-    await board.save();
-
-    return res
-      .status(200)
-      .json({ message: "Board created successfully", board });
-  } catch (err) {
-    console.error(err.message);
-    return res.status(500).json({ message: "Server Error" });
-  }
 });
 
 // Get user's boards
 router.get("/", auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id);
+    try {
+        const user = await User.findById(req.user._id);
 
-    const boards = [];
-    for (const boardId of user.boards) {
-      boards.push(await Board.findById(boardId));
+        const boards = [];
+        for (const boardId of user.boards) {
+            boards.push(await Board.findById(boardId));
+        }
+
+        return res.status(200).json(boards);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ message: "Server Error" });
     }
-
-    return res.status(200).json(boards);
-  } catch (err) {
-    console.error(err.message);
-    return res.status(500).json({ message: "Server Error" });
-  }
 });
 
 // Get a board by id
 router.get("/:id", auth, async (req, res) => {
-  try {
-    const board = await Board.findById(req.params.id);
-    if (!board) return res.status(404).json({ message: "Board not found" });
+    try {
+        const board = await Board.findById(req.params.id);
+        if (!board) return res.status(404).json({ message: "Board not found" });
 
-    return res.status(200).json(board);
-  } catch (err) {
-    return res.status(500).json({ message: "Server Error" });
-  }
+        return res.status(200).json(board);
+    } catch (err) {
+        return res.status(500).json({ message: "Server Error" });
+    }
 });
 
 // Get a board's activity
 router.get("/activity/:boardId", auth, async (req, res) => {
-  try {
-    const board = await Board.findById(req.params.boardId);
-    if (!board) return res.status(404).json({ message: "Board not found" });
+    try {
+        const board = await Board.findById(req.params.boardId);
+        if (!board) return res.status(404).json({ message: "Board not found" });
 
-    return res.status(200).json(board.activity);
-  } catch (err) {
-    console.error(err.message);
-    return res.status(500).json({ message: "Server Error" });
-  }
+        return res.status(200).json(board.activity);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ message: "Server Error" });
+    }
 });
 
 // Rename a board's title
 router.patch(
-  "/rename/:id",
-  [
-    auth,
-    // member
-  ],
-  async (req, res) => {
-    try {
-      const { title } = req.body;
-      if (title === "") {
-        return res.status(400).json({ message: "Title is required" });
-      }
+    "/rename/:id",
+    [
+        auth,
+        // member
+    ],
+    async (req, res) => {
+        try {
+            const { title } = req.body;
+            if (title === "") {
+                return res.status(400).json({ message: "Title is required" });
+            }
 
-      const board = await Board.findById(req.params.id);
-      if (!board) {
-        return res.status(404).json({ message: "Board not found" });
-      }
+            const board = await Board.findById(req.params.id);
+            if (!board) {
+                return res.status(404).json({ message: "Board not found" });
+            }
 
-      // Log activity
-      const user = await User.findById(req.user._id);
-      if (title !== board.title) {
-        board.activity.unshift({
-          text: `${user.name} renamed this board (from '${board.title}')`,
-        });
-      }
+            // Log activity
+            const user = await User.findById(req.user._id);
+            if (title !== board.title) {
+                board.activity.unshift({
+                    text: `${user.name} renamed this board (from '${board.title}')`,
+                });
+            }
 
-      board.title = title;
-      await board.save();
+            board.title = title;
+            await board.save();
 
-      return res
-        .status(200)
-        .json({ message: "Board renamed successfully", board });
-    } catch (err) {
-      console.error(err.message);
-      return res.status(500).json({ message: "Server Error" });
+            return res
+                .status(200)
+                .json({ message: "Board renamed successfully", board });
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).json({ message: "Server Error" });
+        }
     }
-  }
 );
 
 // Add a board member
 router.put(
-  "/add-member/:boardId/:userId",
-  [
-    auth,
-    // member
-  ],
-  async (req, res) => {
-    try {
-      // const board = await Board.findById(req.header("boardId"));
-      const user = await User.findById(req.params.userId);
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
+    "/add-member/:boardId/:userId",
+    [
+        auth,
+        // member
+    ],
+    async (req, res) => {
+        try {
+            // const board = await Board.findById(req.header("boardId"));
+            const user = await User.findById(req.params.userId);
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
 
-      const board = await Board.findById(req.params.boardId);
-      if (!board) {
-        return res.status(404).json({ message: "Board not found" });
-      }
+            const board = await Board.findById(req.params.boardId);
+            if (!board) {
+                return res.status(404).json({ message: "Board not found" });
+            }
 
-      // See if the user is already a member of the board
-      const members = JSON.stringify(
-        board.members.map((member) => member.user)
-      );
-      if (members.includes(req.params.userId)) {
-        return res.status(400).json({ message: "Already member of board" });
-      }
+            // See if the user is already a member of the board
+            const members = JSON.stringify(
+                board.members.map((member) => member.user)
+            );
+            if (members.includes(req.params.userId)) {
+                return res
+                    .status(400)
+                    .json({ message: "Already member of board" });
+            }
 
-      // Add board to user's boards
-      user.boards.unshift(board.id);
-      await user.save();
+            // Add board to user's boards
+            user.boards.unshift(board.id);
+            await user.save();
 
-      // Add user to board's members with 'normal' role
-      board.members.push({ user: user._id, name: user.name, role: "normal" });
+            // Add user to board's members with 'normal' role
+            board.members.push({
+                user: user._id,
+                name: user.name,
+                role: "normal",
+            });
 
-      // Log activity
-      board.activity.unshift({
-        text: `${user.name} joined this board`,
-      });
-      await board.save();
+            // Log activity
+            board.activity.unshift({
+                text: `${user.name} joined this board`,
+            });
+            await board.save();
 
-      return res.status(200).json(board.members);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
+            // return res.status(200).json(board.members);
+            return res.status(200).json({
+                message: `${user.name} joined this board successfully`,
+                boardMembers: board.members,
+            });
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server Error");
+        }
     }
-  }
 );
 
 // Remove a board member
 router.put(
-  "/remove-member/:boardId/:userId",
-  [
-    auth,
-    // member
-  ],
-  async (req, res) => {
-    try {
-      const board = await Board.findById(req.params.boardId);
-      const user = await User.findById(req.params.userId);
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
+    "/remove-member/:boardId/:userId",
+    [
+        auth,
+        // member
+    ],
+    async (req, res) => {
+        try {
+            const board = await Board.findById(req.params.boardId);
+            const user = await User.findById(req.params.userId);
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
 
-      const member = board.members.find(
-        (memberId) => memberId.user == req.user._id
-      );
-      // See if it's the admin logged in now
-      if (member.role != "admin")
-        return res.status(400).json({
-          message: "Only admin is allowed to remove a board member",
-        });
+            const member = board.members.find(
+                (memberId) => memberId.user == req.user._id
+            );
+            // See if it's the admin logged in now
+            if (member.role != "admin")
+                return res.status(400).json({
+                    message: "Only admin is allowed to remove a board member",
+                });
 
-      // Look for the index of the board in the user's board catalog
-      const boardIndex = user.boards.indexOf(
-        user.boards.find((boardId) => boardId == board.id)
-      );
-      // Remove board from user's board
-      user.boards.splice(boardIndex, 1);
-      await user.save();
+            // Look for the index of the board in the user's board catalog
+            const boardIndex = user.boards.indexOf(
+                user.boards.find((boardId) => boardId == board.id)
+            );
+            // Remove board from user's board
+            user.boards.splice(boardIndex, 1);
+            await user.save();
 
-      // Look for the index of the user in the board's members
-      const memberIndex = board.members.indexOf(
-        board.members.find((memberId) => memberId.user == req.params.userId)
-      );
-      // Remove user from board's members
-      board.members.splice(memberIndex, 1);
+            // Look for the index of the user in the board's members
+            const memberIndex = board.members.indexOf(
+                board.members.find(
+                    (memberId) => memberId.user == req.params.userId
+                )
+            );
+            // Remove user from board's members
+            board.members.splice(memberIndex, 1);
 
-      // Log activity
-      board.activity.unshift({
-        text: `${user.name} left this board`,
-      });
-      await board.save();
-      return res.status(200).json(board.members);
-    } catch (err) {
-      return res.status(500).json({ message: "Server Error" });
+            // Log activity
+            board.activity.unshift({
+                text: `${user.name} left this board`,
+            });
+            await board.save();
+            return res.status(200).json(board.members);
+        } catch (err) {
+            return res.status(500).json({ message: "Server Error" });
+        }
     }
-  }
 );
 
 // Probably causes errors
 // Delete a board
 router.delete(
-  "/:id",
-  [
-    auth,
-    // member
-  ],
-  async (req, res) => {
-    try {
-      const board = await Board.findById(req.params.id);
-      if (!board) return res.status(404).json({ message: "Board not found" });
+    "/:id",
+    [
+        auth,
+        // member
+    ],
+    async (req, res) => {
+        try {
+            const board = await Board.findById(req.params.id);
+            if (!board)
+                return res.status(404).json({ message: "Board not found" });
 
-      const member = board.members.find(
-        (memberId) => memberId.user == req.user._id
-      );
-      // See if it's the admin logged in now
-      if (member.role != "admin")
-        return res.status(400).json({
-          message: "Only admin is allowed to delete this board",
-        });
+            const member = board.members.find(
+                (memberId) => memberId.user == req.user._id
+            );
+            // See if it's the admin logged in now
+            if (member.role != "admin")
+                return res.status(400).json({
+                    message: "Only admin is allowed to delete this board",
+                });
 
-      await Board.findByIdAndDelete(req.params.id);
-      return res.status(200).json({ message: "Board deleted successfully" });
-    } catch (err) {
-      return res.status(500).json({ message: "Server Error" });
+            await Board.findByIdAndDelete(req.params.id);
+            return res
+                .status(200)
+                .json({ message: "Board deleted successfully" });
+        } catch (err) {
+            return res.status(500).json({ message: "Server Error" });
+        }
     }
-  }
 );
 
 module.exports = router;
--- api/lists.js
+++ api/lists.js
@@ -10,233 +10,236 @@ const List = require("../models/List");
 
 // Add a list
 router.post(
-  "/:boardId/",
-  [
-    auth,
-    // member
-  ],
-  async (req, res) => {
-    try {
-      const { title } = req.body;
-      if (title === "") {
-        return res.status(400).json({ message: "Title is required" });
-      }
+    "/:boardId/",
+    [
+        auth,
+        // member
+    ],
+    async (req, res) => {
+        try {
+            const { title } = req.body;
+            if (title === "") {
+                return res.status(400).json({ message: "Title is required" });
+            }
 
-      const boardId = req.params.boardId;
+            const boardId = req.params.boardId;
 
-      // Create and save the list
-      const newList = new List({ title });
-      const list = await newList.save();
+            // Create and save the list
+            const newList = new List({ title });
+            const list = await newList.save();
 
-      // Assign the list to the board
-      const board = await Board.findById(boardId);
-      board.lists.push(list.id);
+            // Assign the list to the board
+            const board = await Board.findById(boardId);
+            board.lists.push(list.id);
 
-      // Log activity
-      const user = await User.findById(req.user._id);
-      board.activity.unshift({
-        text: `${user.name} added '${title}' to this board`,
-      });
-      await board.save();
+            // Log activity
+            const user = await User.findById(req.user._id);
+            board.activity.unshift({
+                text: `${user.name} added '${title}' to this board`,
+            });
+            await board.save();
 
-      return res
-        .status(200)
-        .json({ message: "List created successfully", list });
-    } catch (err) {
-      console.error(err.message);
-      return res.status(500).json({ message: "Server Error" });
+            return res
+                .status(200)
+                .json({ message: "List created successfully", list });
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).json({ message: "Server Error" });
+        }
     }
-  }
 );
 
 // Get all of a board's lists
 router.get("/board-lists/:boardId", auth, async (req, res) => {
-  try {
-    const board = await Board.findById(req.params.boardId);
-    if (!board) {
-      return res.status(404).json({ message: "Board not found" });
-    }
+    try {
+        const board = await Board.findById(req.params.boardId);
+        if (!board) {
+            return res.status(404).json({ message: "Board not found" });
+        }
 
-    const lists = [];
-    for (const listId of board.lists) {
-      lists.push(await List.findById(listId));
-    }
+        const lists = [];
+        for (const listId of board.lists) {
+            lists.push(await List.findById(listId));
+        }
 
-    return res.status(200).json(lists);
-  } catch (err) {
-    console.error(err.message);
-    return res.status(500).json({ message: "Server Error" });
-  }
+        return res.status(200).json(lists);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ message: "Server Error" });
+    }
 });
 
 // Get a list by id
 router.get("/:id", auth, async (req, res) => {
-  try {
-    const list = await List.findById(req.params.id);
-    if (!list) {
-      return res.status(404).json({ message: "List not found" });
-    }
+    try {
+        const list = await List.findById(req.params.id);
+        if (!list) {
+            return res.status(404).json({ message: "List not found" });
+        }
 
-    return res.status(200).json(list);
-  } catch (err) {
-    console.error(err.message);
-    return res.status(500).json({ message: "Server Error" });
-  }
+        return res.status(200).json(list);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ message: "Server Error" });
+    }
 });
 
 // Rename a list's title
 router.patch(
-  "/rename/:boardId/:id",
-  [
-    auth,
-    // member
-  ],
-  async (req, res) => {
-    try {
-      const { title } = req.body;
-      if (title === "") {
-        return res.status(400).json({ message: "Title is required" });
-      }
+    "/rename/:boardId/:id",
+    [
+        auth,
+        // member
+    ],
+    async (req, res) => {
+        try {
+            const { title } = req.body;
+            if (title === "") {
+                return res.status(400).json({ message: "Title is required" });
+            }
 
-      const list = await List.findById(req.params.id);
-      if (!list) {
-        return res.status(404).json({ message: "List not found" });
-      }
+            const list = await List.findById(req.params.id);
+            if (!list) {
+                return res.status(404).json({ message: "List not found" });
+            }
 
-      const board = await Board.findById(req.params.boardId);
-      if (!board) {
-        return res.status(404).json({ message: "Board not found" });
-      }
+            const board = await Board.findById(req.params.boardId);
+            if (!board) {
+                return res.status(404).json({ message: "Board not found" });
+            }
 
-      // Log activity
-      const user = await User.findById(req.user._id);
-      if (title !== list.title) {
-        board.activity.unshift({
-          text: `${user.name} renamed the list to '${title}' (from '${list.title}')`,
-        });
-      }
-      await board.save();
+            // Log activity
+            const user = await User.findById(req.user._id);
+            if (title !== list.title) {
+                board.activity.unshift({
+                    text: `${user.name} renamed the list to '${title}' (from '${list.title}')`,
+                });
+            }
+            await board.save();
 
-      list.title = title;
-      await list.save();
+            list.title = title;
+            await list.save();
 
-      return res
-        .status(200)
-        .json({ message: "List renamed successfully", list });
-    } catch (err) {
-      console.error(err.message);
-      return res.status(500).json({ message: "Server Error" });
+            return res
+                .status(200)
+                .json({ message: "List renamed successfully", list });
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).json({ message: "Server Error" });
+        }
     }
-  }
 );
 
 // Archive/Unarchive a list
 router.patch(
-  "/archive/:boardId/:archive/:id",
-  [
-    auth,
-    // member
-  ],
-  async (req, res) => {
-    try {
-      const list = await List.findById(req.params.id);
-      if (!list) {
-        return res.status(404).json({ message: "List not found" });
-      }
+    "/archive/:boardId/:archive/:id",
+    [
+        auth,
+        // member
+    ],
+    async (req, res) => {
+        try {
+            const list = await List.findById(req.params.id);
+            if (!list) {
+                return res.status(404).json({ message: "List not found" });
+            }
 
-      list.archived = req.params.archive === "true";
-      await list.save();
+            list.archived = req.params.archive === "true";
+            await list.save();
 
-      // Log activity
-      const user = await User.findById(req.user._id);
-      const board = await Board.findById(req.params.boardId);
-      board.activity.unshift({
-        text: list.archived
-          ? `${user.name} archived list '${list.title}'`
-          : `${user.name} sent list '${list.title}' to the board`,
-      });
-      await board.save();
+            // Log activity
+            const user = await User.findById(req.user._id);
+            const board = await Board.findById(req.params.boardId);
+            board.activity.unshift({
+                text: list.archived
+                    ? `${user.name} archived list '${list.title}'`
+                    : `${user.name} sent list '${list.title}' to the board`,
+            });
+            await board.save();
 
-      return res.status(200).json(list);
-    } catch (err) {
-      console.error(err.message);
-      return res.status(500).json({ message: "Server Error" });
+            return res.status(200).json(list);
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).json({ message: "Server Error" });
+        }
     }
-  }
 );
 
 // Move a list
 router.patch(
-  "/move/:boardId/:id",
-  [
-    auth,
-    // member
-  ],
-  async (req, res) => {
-    try {
-      const toIndex = req.body.toIndex ? req.body.toIndex : 0;
-      const boardId = req.params.boardId;
-      const listId = req.params.id;
+    "/move/:boardId/:id",
+    [
+        auth,
+        // member
+    ],
+    async (req, res) => {
+        try {
+            const toIndex = req.body.toIndex ? req.body.toIndex : 0;
+            const boardId = req.params.boardId;
+            const listId = req.params.id;
 
-      const board = await Board.findById(boardId);
-      if (!board) {
-        return res.status(404).json({ message: "Board not found" });
-      }
+            const board = await Board.findById(boardId);
+            if (!board) {
+                return res.status(404).json({ message: "Board not found" });
+            }
 
-      const list = await List.findById(listId);
-      if (!list) {
-        return res.status(404).json({ message: "List not found" });
-      }
+            const list = await List.findById(listId);
+            if (!list) {
+                return res.status(404).json({ message: "List not found" });
+            }
 
-      board.lists.splice(board.lists.indexOf(listId), 1);
-      board.lists.splice(toIndex, 0, listId);
-      await board.save();
+            board.lists.splice(board.lists.indexOf(listId), 1);
+            board.lists.splice(toIndex, 0, listId);
+            await board.save();
 
-      return res.status(200).json(board.lists);
-    } catch (err) {
-      console.error(err.message);
-      return res.status(500).json({ message: "Server Error" });
+            return res.status(200).json(board.lists);
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).json({ message: "Server Error" });
+        }
     }
-  }
 );
 
 // Delete a list
 router.delete(
-  "/:boardId/:id",
-  [
-    auth,
-    // member
-  ],
-  async (req, res) => {
-    try {
-      const list = await List.findById(req.params.id);
-      if (!list) return res.status(404).json({ message: "List not found" });
+    "/:boardId/:id",
+    [
+        auth,
+        // member
+    ],
+    async (req, res) => {
+        try {
+            const list = await List.findById(req.params.id);
+            if (!list)
+                return res.status(404).json({ message: "List not found" });
 
-      const board = await Board.findById(req.params.boardId);
-      if (!board) {
-        return res.status(404).json({ message: "Board not found" });
-      }
+            const board = await Board.findById(req.params.boardId);
+            if (!board) {
+                return res.status(404).json({ message: "Board not found" });
+            }
 
-      // Look for the index of the list in the board's lists
-      const listIndex = board.lists.indexOf(
-        board.lists.find((list) => list.id == req.params.id)
-      );
-      // Remove list from board's lists
-      board.lists.splice(listIndex, 1);
+            // Look for the index of the list in the board's lists
+            const listIndex = board.lists.indexOf(
+                board.lists.find((list) => list.id == req.params.id)
+            );
+            // Remove list from board's lists
+            board.lists.splice(listIndex, 1);
 
-      // Log activity
-      const user = await User.findById(req.user._id);
-      board.activity.unshift({
-        text: `${user.name} removed the list '${list.title}' from this board`,
-      });
-      await board.save();
+            // Log activity
+            const user = await User.findById(req.user._id);
+            board.activity.unshift({
+                text: `${user.name} removed the list '${list.title}' from this board`,
+            });
+            await board.save();
 
-      await List.findByIdAndDelete(req.params.id);
-      return res.status(200).json({ message: "List deleted successfully" });
-    } catch (err) {
-      return res.status(500).json({ message: "Server Error" });
+            await List.findByIdAndDelete(req.params.id);
+            return res
+                .status(200)
+                .json({ message: "List deleted successfully" });
+        } catch (err) {
+            return res.status(500).json({ message: "Server Error" });
+        }
     }
-  }
 );
 
 module.exports = router;
